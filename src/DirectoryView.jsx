32ec7c6c77480800080824f8e0c957d24e8eb99a
Janak Bahadur Bohara <janakcustomx@gmail.com>
2025-12-04 18:25:05 +0545

    update

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -27,7 +27,7 @@ function DirectoryView() {
   const { dirId } = useParams();
   const navigate = useNavigate();
 
-  const [directoryName, setDirectoryName] = useState("My Home!!!");
+  const [directoryName, setDirectoryName] = useState("My Homepage");
   const [directoriesList, setDirectoriesList] = useState([]);
   const [filesList, setFilesList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
@@ -58,7 +58,7 @@ function DirectoryView() {
   const loadDirectory = async () => {
     try {
       const data = await getDirectoryItems(dirId);
-      setDirectoryName(dirId ? data.name : "My Home!!!");
+      setDirectoryName(dirId ? data.name : "My Homepage");
       setDirectoriesList([...data.directories].reverse());
       setFilesList([...data.files].reverse());
     } catch (err) {
